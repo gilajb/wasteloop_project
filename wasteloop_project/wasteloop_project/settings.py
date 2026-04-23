d96fdfb53fe19a8230d58e09d89d31608375b553
gilajb <cjoybett@gmail.com>
2026-04-23 13:13:22 +0300

    add whitenoise, remove pycache from tracking

--- wasteloop_project/wasteloop_project/settings.py
+++ wasteloop_project/wasteloop_project/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,6 +85,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']   # global static folder
 STATIC_ROOT = BASE_DIR / 'staticfiles'      # collectstatic output
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
